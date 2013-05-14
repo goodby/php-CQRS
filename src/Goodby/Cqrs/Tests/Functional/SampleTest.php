73c6c3c82a713d3d884a0c48717627ef73bcb159
suin <suinyeze@gmail.com>
2013-05-14 13:06:12 +0900

    set up project

---
+++ src/Goodby/Cqrs/Tests/Functional/SampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Goodby\Cqrs\Tests\Functional;
+
+class SampleTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSample()
+    {
+        $this->assertTrue(false);
+    }
+}
